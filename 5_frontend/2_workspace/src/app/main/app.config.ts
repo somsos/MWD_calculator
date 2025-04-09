791f91206c24da33a964543162f86979dbb2266e
mario <somsos112@gmail.com>
2025-04-08 18:42:33 -0600

    enabled basic PWA

--- 5_frontend/2_workspace/src/app/main/app.config.ts
+++ 5_frontend/2_workspace/src/app/main/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideAnimationsAsync('noop'),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ]
 };
